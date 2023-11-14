d73b591e6a0ee25eb05284148a80b59459d31d1e
Alex Bashara <alexbashara2002@gmail.com>
2023-11-14 02:41:13 -0600

    WIP CAN CMSIS Library

--- Examples/CAN_Test/Core/Inc/can.h
+++ Examples/CAN_Test/Core/Inc/can.h
@@ -1,17 +1,17 @@
 /* Header for CAN configuration
  * This file handles all of the configuration of the CAN peripherals
  * The order should be
- * Clocks
  * Bit timing
  * Global receive -- try with everything first
  * CCCR register --> enable FDCAN
  */
 
-#include "cmsis_os.h"
+#include "stm32h743xx.h"
 
-
-static inline can_clock() {
-
+static inline can_bit_timming() {
+	FDCAN1->CCCR = FDCAN_CCCR_CCE;
+	FDCAN1->CCCR = FDCAN_CCCR_INIT;
+	FDCAN1->DBTP = (20UL << FDCAN_DBTP_DBRP_Pos);
 }
 
 static inline can_cc() {
