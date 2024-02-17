016e24ca127371f58b1c0280c19328ff9575fbec
apbashara <alexbashara2002@gmail.com>
2024-02-17 15:56:15 -0600

    WIP: UART Driver

---
+++ Examples/CMSIS_UART/Core/Inc/uart.h
@@ -0,0 +1,60 @@
+#include "stm32h743xx.h"
+
+/**
+ * @brief Initialize UART3
+ * @note Baud rate: 115200, 8 data bits, 1 stop bit, no parity
+ * @note Configured for 8Mhz Peripherial Clock
+ * @note TODO: Look into FIFO Mode operation
+ * @return 0 if successful, -1 if unsuccessful
+ */
+uint8_t static inline UART3_Init(void) {
+    RCC->APB1LENR |= RCC_APB1LENR_USART3EN; // Enable USART3 clock
+    USART3->CR1 &= USART_CR1_UE; // Make sure USART3 is disabled to configure
+
+    // Set word length to 8 bits '00' in M0 and M1
+    USART3->CR1 &= (0UL << USART_CR1_M1_Pos);
+    USART3->CR1 &= (0UL << USART_CR1_M0_Pos);
+
+    // Set Baud Rate to 115200
+    USART3->BRR = (0x45 << USART_BRR_DIV_MANTISSA_Pos); // See pg 2037 of RM0433 Reference Manual
+    USART3->CR2 &= (0UL << USART_CR2_STOP_Pos); // Set stop bits to 1
+    USART3->CR1 |= USART_CR1_UE; // Enable USART3
+    return 0;
+}
+
+/**
+ * @brief Sends a character over UART
+ * @note Page 2027 of RM0433 Reference Manual describes how to send data
+ * @param ch character to send
+ * @return 0 if successful, -1 if unsuccessful
+ */
+uint8_t static inline UART_SendChar(uint8_t data) {
+    // Can only write to TDR when TXE/TXFNF is set
+    USART3->CR1 |= USART_CR1_TE; // Enable transmitter
+    USART3->TDR = data; // Write to TDR
+
+    // TODO: TXE flag not being cleared
+    while ((USART3->ISR & USART_ISR_TXE_TXFNF)); // Wait for Transmit Data Register Empty flag
+    while (!(USART3->ISR & USART_ISR_TC)); // Wait for Transmission Complete flag
+    USART3->CR1 &= ~USART_CR1_TE; // Disable transmitter
+    return 0;
+}
+
+/**
+ * @brief Sends a String over UART
+ * @note Calls UART_SendChar for each character in the string
+ * @param str string value to send
+ * @return uint8_t 
+ */
+uint8_t static inline UART_SendString(char *str) {
+    for (int i = 0; i < sizeof(str); i++){
+        UART_SendChar(str[i]);
+    }
+    return 0;
+}
+void static inline UART_ReceiveChar(void) {
+
+}
+void static inline UART_ReceiveString(char *str) {
+
+}
---
+++ Examples/CMSIS_UART/Core/Src/main.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include "stm32h743xx.h"
+#include "cmsis_os.h"
+#include "uart.h"
+#include "gpio.h"
+
+uint8_t data[] = "Hello World!\n"; // 8 Bit Buffer for text data
+
+void BlinkStatusTask(void *argument);
+void UARTTask(void *argument);
+
+osThreadId_t BlinkStatusHandle;
+const osThreadAttr_t BlinkStatus_attributes = {
+  .name = "BlinkStatus",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 128 * 4
+};
+
+osThreadId_t UARTHandle;
+const osThreadAttr_t UART_attributes = {
+  .name = "UART",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+
+
+int main() {
+  // Initialize UART3
+  UART3_Init();
+  GPIOB_Init();
+  osKernelInitialize();
+
+  BlinkStatusHandle = osThreadNew(BlinkStatusTask, NULL, &BlinkStatus_attributes);
+  UARTHandle = osThreadNew(UARTTask, NULL, &UART_attributes);
+
+  osKernelStart();
+  
+  while(1) {}
+  return 0;
+}
+
+void BlinkStatusTask(void *argument) {
+  while(1) {
+    // Toggle LED
+    Toggle_Pin(GPIOB, 14);
+    osDelay(1000);
+  }
+}
+
+void UARTTask(void *argument) {
+  while(1) {
+    // Send data to UART3
+    for (int i = 0; i < sizeof(data); i++){
+      UART_SendChar(data[i]);
+    }
+    osDelay(500);
+  }
+}
