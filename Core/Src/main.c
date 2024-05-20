681d78d1620a41e9d958b17182ea5691cc00b9b8
APBashara <alexbashara2002@gmail.com>
2024-05-20 00:00:04 -0500

    WIP Timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,6 +1,74 @@
 #include "stm32f407xx.h"
 #include "cmsis_os2.h"
 
+void SysClock_Config();
+
+void LED_Init() {
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable GPIO D Clock
+
+  // Set LED Pins to Output mode
+  // Pins reset to push pull mode
+  GPIOD->MODER &= ~GPIO_MODER_MODE12 & ~GPIO_MODER_MODE13 
+                & ~GPIO_MODER_MODE14 & ~GPIO_MODER_MODE15; // Clear LED Pins
+  GPIOD->MODER |= (0b01 << GPIO_MODER_MODE12_Pos) | (0b01 << GPIO_MODER_MODE13_Pos) 
+                | (0b01 << GPIO_MODER_MODE14_Pos) | (0b01 << GPIO_MODER_MODE15_Pos); // Set LED Pins to output
+}
+
+void Toggle_Pin(int pin) {
+  GPIOD->ODR ^= (1 << pin);
+}
+
+void Delay_Temp() {
+  for (int i = 0; i < 1000000; i++) {
+    __NOP();
+  }
+}
+
+void TIM2_IRQHandler(void) {
+    static uint32_t i = 1;
+
+    // clear interrupt status
+    if (TIM2->DIER & TIM_DIER_CC1IE) { // Capture Compare 1 Interrupt
+        if (TIM2->SR & TIM_SR_CC1IF) { // Capture Compare 1 Flag
+            TIM2->SR &= ~(1U << TIM_SR_UIF); // Clear Update Interrupt Flag
+        }
+    }
+    Toggle_Pin(15); // Flash LED
+}
+
+/**
+ * @brief Initialize Timer 2
+ * @note Timer 2 runs at APB1(42Mhz) * 2 = 84Mhz
+ * @note Timer runs at fCK_PSC / (PSC + 1)
+ */
+void TIM2_Init() {
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Enable TIM2 Clock
+
+  TIM2->PSC = 8400 - 1; // Set Prescaler to 8399 (10KHz)
+  TIM2->ARR = 1000; // Set Auto Reload Register to 1000
+
+  TIM2->DIER |= TIM_DIER_UIE; // Enable Update Interrupt
+  NVIC_SetPriority(TIM2_IRQn, 2); // Set Priority to 2
+  NVIC_EnableIRQ(TIM2_IRQn); // Enable TIM2 Interrupt
+
+  TIM2->CR1 |= TIM_CR1_CEN; // Enable Timer
+}
+
+int main() {
+  SysClock_Config();
+  LED_Init();
+
+  while(1) {
+    Toggle_Pin(12);
+    osDelay(1000);
+    Toggle_Pin(13);
+    osDelay(1000);
+    Toggle_Pin(14);
+    osDelay(1000);
+  }
+  return 0;
+}
+
 void SysClock_Config() {
   RCC->CR |= RCC_CR_HSEON; // Enable HSE Clock
   while (!(RCC->CR & RCC_CR_HSERDY)); // Wait until HSE is ready
@@ -30,42 +98,4 @@ void SysClock_Config() {
   while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)); // Wait until PLL is System Clock
   
   SystemCoreClock = 168000000; // Set System Clock to 168MHz
-}
-
-void LED_Init() {
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable GPIO D Clock
-
-  // Set LED Pins to Output mode
-  // Pins reset to push pull mode
-  GPIOD->MODER &= ~GPIO_MODER_MODE12 & ~GPIO_MODER_MODE13 
-                & ~GPIO_MODER_MODE14 & ~GPIO_MODER_MODE15; // Clear LED Pins
-  GPIOD->MODER |= (0b01 << GPIO_MODER_MODE12_Pos) | (0b01 << GPIO_MODER_MODE13_Pos) 
-                | (0b01 << GPIO_MODER_MODE14_Pos) | (0b01 << GPIO_MODER_MODE15_Pos); // Set LED Pins to output
-}
-
-void Toggle_Pin(int pin) {
-  GPIOD->ODR ^= (1 << pin);
-}
-
-void Delay_Temp() {
-  for (int i = 0; i < 1000000; i++) {
-    __NOP();
-  }
-}
-
-int main() {
-  SysClock_Config();
-  LED_Init();
-
-  while(1) {
-    Toggle_Pin(12);
-    Delay_Temp();
-    Toggle_Pin(13);
-    Delay_Temp();
-    Toggle_Pin(14);
-    Delay_Temp();
-    Toggle_Pin(15);
-    Delay_Temp();
-  }
-  return 0;
 }
