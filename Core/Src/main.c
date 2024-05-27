70f4cd237a548120f7e499ebf7d2ff584a64afbf
APBashara <alexbashara2002@gmail.com>
2024-05-26 21:42:56 -0700

    Working USART

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,6 +1,8 @@
 #include "stm32f407xx.h"
 #include "cmsis_os2.h"
 
+#define HSE_VALUE 8000000U
+
 void SysClock_Config();
 
 void LED_Init() {
@@ -59,6 +61,8 @@ void USART2_Init() {
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // Enable USART2 Clock
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable GPIO A Clock
 
+  USART2->CR1 &= ~USART_CR1_UE; // Disable USART
+
   GPIOA->MODER &= ~GPIO_MODER_MODE2 & ~GPIO_MODER_MODE3; // Clear PA2 and PA3
   GPIOA->MODER |= (0x2 << GPIO_MODER_MODE2_Pos) 
                 | (0x2 << GPIO_MODER_MODE3_Pos); // Set PA2 and PA3 to TX and RX
@@ -68,9 +72,20 @@ void USART2_Init() {
 
   GPIOA->AFR[0] |= (0x7 << GPIO_AFRL_AFSEL2_Pos) 
                   | (0x7 << GPIO_AFRL_AFSEL3_Pos); // Set PA2 and PA3 to AF7 (USART2)
+  
+  USART2->CR1 |= USART_CR1_TE | USART_CR1_RE; // Enable Transmitter and Receiver
+
+  // baud rate = fCK / (8 * (2 - OVER8) * USARTDIV)
+  // Page 989 of Reference Manual
+  // 115200 @ 42MHz = 22.8125
+  USART2->BRR |= (0xD << USART_BRR_DIV_Fraction_Pos);
+  USART2->BRR |= (0x16 << USART_BRR_DIV_Mantissa_Pos);
+
+  USART2->CR1 |= USART_CR1_UE; // Enable USART
 }
 
 int main() {
+  uint8_t message[] = "Hello World!\n";
   SysClock_Config();
   LED_Init();
   TIM2_Init();
@@ -83,6 +98,11 @@ int main() {
     Delay_Temp();
     Toggle_Pin(14);
     Delay_Temp();
+    for (uint16_t i = 0; i < sizeof(message); i++) {
+      USART2->DR = message[i];
+      while (!(USART2->SR & USART_SR_TC));
+    }
+    
   }
   return 0;
 }
