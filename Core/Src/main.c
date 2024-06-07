117ef49925c91f27771815392621d9eec232fd58
APBashara <alexbashara2002@gmail.com>
2024-06-06 23:00:44 -0700

    Working Multiple ADC Conversions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -9,7 +9,7 @@
 
 #define HSE_VALUE 8000000U
 
-volatile int adc_value = 0;
+volatile uint16_t adc_value[3];;
 
 void SysClock_Config();
 
@@ -50,8 +50,7 @@ void TIM2_IRQHandler(void) {
 void ADC_Init() {
   ADC->CCR |= (0x1 << ADC_CCR_ADCPRE_Pos); // Set ADC Prescaler to 4 (84MHz / 4 = 21MHz)
   RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // Enable ADC1 Clock
-
-  ADC1->CR2 |= ADC_CR2_ADON; // Enable ADC
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable GPIO A Clock
 
   // Page 272 for GPIO Configuration
   // Set to Analog Mode and disable Pull-up Pull-down
@@ -62,9 +61,16 @@ void ADC_Init() {
   GPIOA->MODER |= (0x3 << GPIO_MODER_MODE5_Pos); 
   GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR5; 
 
-  ADC1->SQR1 &= (0x2 << ADC_SQR1_L_Pos); // Set Regular Sequence Length to 3
-  ADC1->SQR3 &= ~ADC_SQR3_SQ1; // Set Regular Sequence 1 to Channel 1
-  ADC1->SQR3 |= (0x1 << ADC_SQR3_SQ1_Pos); // Set Regular Sequence 1 to Channel 1
+  ADC1->SQR1 |= (0x2 << ADC_SQR1_L_Pos); // Set Regular Sequence Length to 3
+  ADC1->SQR3 |= (0x1 << ADC_SQR3_SQ1_Pos) // Set Regular Sequence 1 to Channel 1
+              | (0x4 << ADC_SQR3_SQ2_Pos) // Set Regular Sequence 2 to Channel 4
+              | (0x5 << ADC_SQR3_SQ3_Pos); // Set Regular Sequence 3 to Channel 5
+
+  ADC1->CR1 |= ADC_CR1_SCAN; // Enable Scan Mode
+  ADC1->CR2 |= ADC_CR2_EOCS; // Enable End of Conversion Selection
+  // ADC1->CR2 |= ADC_CR2_CONT; // Enable Continuous Conversion Mode
+
+  ADC1->CR2 |= ADC_CR2_ADON; // Enable ADC
 }
 
 /**
@@ -74,12 +80,14 @@ void ADC_Init() {
  */
 void ADC_Read() {
   ADC1->CR2 |= ADC_CR2_SWSTART; // Start Conversion
-  while (!(ADC1->SR & ADC_SR_EOC)); // Wait for End of Conversion
-  adc_value = ADC1->DR; // Return the Data Register
+
+  for (int i = 0; i < 3; i++) {
+    while (!(ADC1->SR & ADC_SR_EOC)); // Wait for End of Conversion
+    adc_value[i] = ADC1->DR; // Return the Data Register
+  }
 }
 
 int main() {
-  uint8_t message[] = "Hello World!\n";
   uint8_t ADC_Val[32];
   SysClock_Config();
   LED_Init();
@@ -89,14 +97,14 @@ int main() {
 
   while(1) {
     ADC_Read();
-    if (adc_value > 250) {
+    if (adc_value[0] > 250) {
       GPIOD->ODR |= (1 << 12);
       GPIOD->ODR &= ~(1 << 13);
     } else {
       GPIOD->ODR &= ~(1 << 12);
       GPIOD->ODR |= (1 << 13);
     }
-    sprintf(ADC_Val, "ADC: %d\n", adc_value);
+    sprintf(ADC_Val, "%d, %d, %d\n", adc_value[0], adc_value[1], adc_value[2]);
     send_String(ADC_Val);
   }
   return 0;
