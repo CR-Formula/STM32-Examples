13d69a3dda25590054a45fed84e70a176d253815
APBashara <alexbashara2002@gmail.com>
2024-06-09 18:12:34 -0700

    Added Header File for Clock Configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+#include "sysclk.h"
 #include "uart.h"
 #include "timer.h"
 #include "gpio.h"
@@ -58,7 +59,7 @@ void TIM2_IRQHandler(void) {
 
 int main() {
   uint8_t ADC_Val[32]; // Buffer for print messages
-  SysClock_Config();
+  Sysclock_168(); // Initalize 
   LED_Init();
   TIM2_Init();
   USART3_Init();
@@ -78,39 +79,4 @@ int main() {
     send_String(USART3, ADC_Val);
   }
   return 0;
-}
-
-/**
- * @brief Configure the System Clock to 168MHz
- * 
- */
-void SysClock_Config() {
-  RCC->CR |= RCC_CR_HSEON; // Enable HSE Clock
-  while (!(RCC->CR & RCC_CR_HSERDY)); // Wait until HSE is ready
-
-  RCC->APB1ENR |= RCC_APB1ENR_PWREN; // Enable Power Interface Clock
-  PWR->CR |= PWR_CR_VOS; // Set Scale 1 mode (max clock frequency)
-
-  RCC->CFGR |= RCC_CFGR_HPRE_DIV1; // Set AHB Prescaler to 1
-  RCC->CFGR |= RCC_CFGR_PPRE1_DIV4; // Set APB1 Prescaler to 4
-  RCC->CFGR |= RCC_CFGR_PPRE2_DIV2; // Set APB2 Prescaler to 2
-
-  RCC->PLLCFGR = 7 << RCC_PLLCFGR_PLLQ_Pos; // Set PLLQ to 7
-  RCC->PLLCFGR |= 0 << RCC_PLLCFGR_PLLP_Pos; // Set PLLP to 2
-  RCC->PLLCFGR |= 336 << RCC_PLLCFGR_PLLN_Pos; // Set PLLN to 336
-  RCC->PLLCFGR |= 8 << RCC_PLLCFGR_PLLM_Pos; // Set PLLM to 8
-  RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE; // Set PLL Source to HSE
-
-  FLASH->ACR |= FLASH_ACR_PRFTEN; // Enable Prefetch Buffer
-  FLASH->ACR |= FLASH_ACR_ICEN; // Enable Instruction Cache
-  FLASH->ACR |= FLASH_ACR_DCEN; // Enable Data Cache
-  FLASH->ACR |= FLASH_ACR_LATENCY_5WS; // Set Flash Latency to 5 Wait States
-
-  RCC->CR |= RCC_CR_PLLON; // Enable PLL
-  while (!(RCC->CR & RCC_CR_PLLRDY)); // Wait until PLL is ready
-
-  RCC->CFGR |= RCC_CFGR_SW_PLL; // Set PLL as System Clock
-  while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)); // Wait until PLL is System Clock
-  
-  SystemCoreClock = 168000000; // Set System Clock to 168MHz
 }
