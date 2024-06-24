9d7d994002810ff01eb5657e6b8e7378b607b79a
APBashara <alexbashara2002@gmail.com>
2024-06-24 00:00:40 -0700

    WIP SPI Driver 	NSS Issue 	Data not decoding

--- Core/Inc/gpio.h
+++ Core/Inc/gpio.h
@@ -9,3 +9,23 @@
 void static inline Toggle_Pin(GPIO_TypeDef* GPIO, uint8_t pin) {
   GPIO->ODR ^= (1 << pin);
 }
+
+/**
+ * @brief Set a given GPIO pin high (Set to 1)
+ * 
+ * @param GPIO [GPIO_TypeDef*] GPIO Port to use
+ * @param pin [uint8_t] Pin to set high
+ */
+void static inline Set_Pin(GPIO_TypeDef* GPIO, uint8_t pin) {
+  GPIO->ODR |= (1 << pin);
+}
+
+/**
+ * @brief Clear a given GPIO pin (Set to 0)
+ * 
+ * @param GPIO [GPIO_TypeDef*] GPIO Port to use
+ * @param pin [uint8_t] Pin to set low
+ */
+void static inline Clear_Pin(GPIO_TypeDef* GPIO, uint8_t pin) {
+  GPIO->ODR &= ~(1 << pin);
+}
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,7 @@ volatile uint16_t adc_buffer[16];
 void SysClock_Config();
 void ADCRead(void *argument);
 void USART_Print(void *argument);
+void SPI_Send(void *argument);
 void Status_LED(void *argument);
 
 
@@ -41,6 +42,13 @@ const osThreadAttr_t ADC_Read_Attr = {
 osThreadId_t USART_Print_Handle;
 const osThreadAttr_t USART_Print_Attr = {
   .name = "USART_Print",
+  .stack_size = 128 * 4,
+  .priority = osPriorityNormal
+};
+
+osThreadId_t SPI_Send_Handle;
+const osThreadAttr_t SPI_Attr = {
+  .name = "SPI_Send",
   .stack_size = 128 * 4,
   .priority = osPriorityNormal
 };
@@ -95,34 +103,47 @@ void TIM2_IRQHandler(void) {
 }
 
 /**
- * @brief 
+ * @brief Initialize SPI2
  * 
  */
-void SPI_Init() {
+void SPI2_Init() {
   RCC->APB1ENR |= RCC_APB1ENR_SPI2EN; // Enable SPI2 Clock
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // Enable GPIOB Clock
 
   SPI2->CR1 &= ~SPI_CR1_SPE; // Disable SPI
 
-  GPIOB->MODER &= ~GPIO_MODER_MODE13 & ~GPIO_MODER_MODE14 
-                & ~GPIO_MODER_MODE15; // Clear PB13, PB14, and PB15
-  GPIOB->MODER |= (0x2 << GPIO_MODER_MODE13_Pos) 
+  GPIOB->MODER &= ~GPIO_MODER_MODE12 & ~GPIO_MODER_MODE13
+                & ~GPIO_MODER_MODE14 & ~GPIO_MODER_MODE15; // Clear PB13, PB14, and PB15
+  GPIOB->MODER |= (0x2 << GPIO_MODER_MODE12_Pos)
+                | (0x2 << GPIO_MODER_MODE13_Pos) 
                 | (0x2 << GPIO_MODER_MODE14_Pos) 
-                | (0x2 << GPIO_MODER_MODE15_Pos); // Set PB13, PB14, and PB15 to AF
-  GPIOB->AFR[1] |= (0x5 << GPIO_AFRH_AFSEL13_Pos) 
+                | (0x2 << GPIO_MODER_MODE15_Pos); // Set PB12, PB13, PB14, and PB15 to AF
+  GPIOB->AFR[1] |= (0x5 << GPIO_AFRH_AFSEL12_Pos)
+                | (0x5 << GPIO_AFRH_AFSEL13_Pos) 
                 | (0x5 << GPIO_AFRH_AFSEL14_Pos) 
-                | (0x5 << GPIO_AFRH_AFSEL15_Pos); // Set PB13, PB14, and PB15 to AF5 (SPI1)
+                | (0x5 << GPIO_AFRH_AFSEL15_Pos); // Set PB12, PB13, PB14, and PB15 to AF5 (SPI2)
 
-  SPI2->CR1 |= (0x0 << SPI_CR1_BR_Pos); // Set Baud Rate to fPCLK/2 = 21MHz
-  SPI2->CR1 |= SPI_CR1_CPOL; // Set Clock Polarity to 1
-  SPI2->CR1 &= ~SPI_CR1_CPHA; // Set Clock Phase to 1
-  SPI2->CR1 |= SPI_CR1_DFF; // Set Data Frame Format to 16-bit
-  SPI2->CR1 &= ~SPI_CR1_LSBFIRST; // Set MSB First
-  SPI2->CR1 |= SPI_CR1_SSM; // Set Software Slave Management
-  SPI2->CR1 |= SPI_CR1_SSI; // Set Internal Slave Select
-  SPI2->CR2 &= ~SPI_CR2_FRF; // Set Frame Format to Motorola
-  SPI2->CR1 |= SPI_CR1_MSTR; // Set Master Mode
-  SPI2->CR1 |= SPI_CR1_SPE; // Enable SPI
+  SPI2->CR1 = (0x4 << SPI_CR1_BR_Pos); // Set Baud Rate to fPCLK/32
+
+  // Set CPOL = 1, SSM to software, SSI to high
+  SPI2->CR1 |= SPI_CR1_CPOL | SPI_CR1_SSM 
+            | SPI_CR1_SSI; // Set CPOL, DFF, SSM, and SSI
+
+  // Set CPHA = 0, MSB First, Frame Format = Motorola, 8-bit Data
+  SPI2->CR1 &= ~SPI_CR1_CPHA & ~SPI_CR1_LSBFIRST
+            & ~SPI_CR2_FRF & ~SPI_CR1_DFF;
+  
+  SPI2->CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE; // Set Master and Enable
+}
+
+uint8_t SPI_Write(SPI_TypeDef* SPI, uint8_t data, uint8_t device) {
+  Toggle_Pin(GPIOB, 12); // Pull GPIOB12 High for CS
+  while (!(SPI->SR & SPI_SR_TXE)); // Wait until TXE is set
+  SPI->DR = data; // Write data to DR
+  while (!(SPI->SR & SPI_SR_RXNE)); // Wait until RXNE is set
+  uint8_t Read = SPI->DR; // Read DR to clear RXNE
+  Toggle_Pin(GPIOB, 12); // Pull GPIOB12 Low for CS
+  return Read;
 }
 
 /**
@@ -139,12 +160,14 @@ int main() {
   USART3_Init();
   ADC_Init();
   DMA_ADC1_Init(adc_buffer);
+  SPI2_Init();
 
   osKernelInitialize(); // Initialize FreeRTOS
 
   ADC_Read_Handle = osThreadNew(ADCRead, NULL, &ADC_Read_Attr);
   USART_Print_Handle = osThreadNew(USART_Print, NULL, &USART_Print_Attr);
   Status_LED_Handle = osThreadNew(Status_LED, NULL, &Status_LED_Attr);
+  SPI_Send_Handle = osThreadNew(SPI_Send, NULL, &SPI_Attr);
 
   osKernelStart(); // Start FreeRTOS
 
@@ -188,6 +211,18 @@ void USART_Print(void *argument) {
     osDelay(1000);
     sprintf(message, "Hello World!\n");
     send_String(USART3, message);
+  }
+}
+
+/**
+ * @brief Send SPI Message over SPI2
+ * 
+ * @param argument 
+ */
+void SPI_Send(void *argument) {
+  while(1) {
+    SPI_Write(SPI2, 0xAA, 12);
+    osDelay(100);
   }
 }
 
